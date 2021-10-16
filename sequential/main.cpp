a562f222f75a7d445a6c46a4d6e9e7907e6d29b5
yuejiewang <kathywangyuejie@gmail.com>
2021-10-16 13:06:41 -0400

    add cmdline parse

--- sequential/main.cpp
+++ sequential/main.cpp
@@ -1,18 +1,32 @@
 #include<stdlib.h>
 #include<stdio.h>
+#include<unistd.h>
 #include"../utils.hpp"
 #include"./utils.hpp"
 using namespace std;
 
-int main()
+int main(int argc, char** argv)
 {
-    axis_t pow = 4;
+    char* pstr = NULL;
+    int c;
+    while ((c = getopt(argc, argv, "p:")) != -1)
+        switch (c)
+        {
+        case 'p':
+            pstr = optarg;
+            break;
+        default:
+            abort();
+        }
+    
+    axis_t pow;
+    sscanf(pstr, "%lu", &pow);
     axis_t N = (1 << pow);
     index_t** mat = new index_t* [N];
     for (index_t i = 0; i < N; i++) {
         mat[N] = new index_t [N];
     }
-
+    
     for (axis_t x = 0; x < N; x++) {
         for (axis_t y = 0; y < N; y++) {
             index_t hidx = index2hilbert_2d(x, y, pow);
@@ -20,7 +34,7 @@ int main()
             index_2d idx = hilbert2index_2d(hidx, pow);
             if (idx.row != x || idx.col != y) {
                 printf("reverse convert failed at %llu, %lu, %lu\n", hidx, idx.row, idx.col);
-                exit(1);
+                abort();
             }
         }
         printf("\n");
